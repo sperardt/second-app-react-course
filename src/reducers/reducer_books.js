00a84dd28caa6f10eddc8bef94fbfa21298ea165
Sabrina Perardt <sabrinaperardt@DANs-MBP.hitronhub.home>
2018-04-27 16:20:06 -0700

    book list and details

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title:'harry potter', page: 40},
+    {title: 'lean in', page: 340},
+    {title: 'Game of thrones', page: 140},
+    {title: 'hunter games', page: 1140}
+  ]
+}
